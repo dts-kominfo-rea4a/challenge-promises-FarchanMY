f83f8e6bcfdaa0cfb84723daa3277ebec91c3610
Farchan Mungis Yudistira <farhan.mungis@gmail.com>
2022-09-19 21:32:55 +0700

    TASK DAY 2 DTS-KOMINFO-REA4

--- promise.js
+++ promise.js
@@ -1,13 +1,13 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promis eOutput sesuai ketentuan readme
-let bioskopMarah = 0;
-let bioskopTidakMarah =0;
-const promiseOutput = emosi =>{
-  if (emosi === 'marah' || emosi=== 'tidak marah'){
+let bioskopMarah = 1;
+let bioskopTidakMarah = -1;
+const promiseOutput = (emosi1,emosi2) =>{
+  if (emosi1 === 'marah' || emosi2=== 'tidak marah'){
     return promiseTheaterIXX().then((value) =>{
       value.forEach((array) => {
-        if (array.hasil === 'marah'|| array.hasil === 'marah'){
+        if (array.hasil === 'marah'|| array.hasil === 'tidak marah'){
          bioskopMarah++ && bioskopTidakMarah++;
         } else{
           'error'
